5597dce736960eb716535dea15e033a221181e91
palyla <palyla@ya.ru>
2019-12-06 00:46:19 +0300

    notes

--- app.c
+++ app.c
@@ -7,26 +7,27 @@
 #include "uart.h"
 
 #define TIMER1_TICK_US 46
-#define TIMER2_TICKS_EQ_A_SECOND 100
+#define TIMER2_TICKS_EQ_A_SECOND 90
 
 
 #define FUEL_ML_SPENT_US 0.0000032
 
-#define MILLILITERS_IN_MINUTE 191.8
+#define MILLILITERS_IN_MINUTE 191.8L
 #define MILLILITERS_IN_MICROS (MILLILITERS_IN_MINUTE/(60*1000*2))
 
 #define LITTERS_IN_HOUR ((MILLILITERS_IN_MINUTE*60) / 1000)
-#define LITTERS_IN_HOUR_FROM_MILLILITERS_IN_MICROS(X) ((X*1000*2*60*60)/1000)
+#define LITTERS_IN_HOUR_FROM_MILLILITERS_IN_MICROS(X) ((X*1000*2*60*60)/1000L)
 
 
 
 // 1 tick == 46 micros
 static volatile uint64_t timer1_ticks = 0;
-// 100 times overflow == 1 secs
+// 90 times overflow == 1 secs
 static volatile uint64_t timer2_ticks = 0;
 
 
-ISR(TIMER2_OVF) {
+ISR(TIMER2_OVF_vect) {
+	// printf("timer2\n");
 	timer2_ticks += 1;
 }
 
@@ -47,16 +48,9 @@ void pin_init(void) {
     EIMSK |= (1 << INT1);
 }
 
-uint64_t spent_fuel(uint64_t opened_us) {
-	uint64_t opened_us = timer1_ticks * TIMER1_TICK_US;
-
-
-	return ;
-}
-
 // 3.2 millititers in secound
 int main(void) {
-	uint64_t spent_ml = 0; /* milliliters in microsecound */
+	long double spent_ml = 0; /* milliliters in microsecound */
 
 
 	uart_init();
@@ -66,6 +60,7 @@ int main(void) {
 	pin_init();
 
     TIMSK2 |= (1 << TOIE2);
+    TIFR2 |= (1 << TOV2);
     TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
 
     sei();
@@ -74,8 +69,10 @@ int main(void) {
 	    while (timer2_ticks < TIMER2_TICKS_EQ_A_SECOND)
 	    	;
 
+	    printf("ddddddddddddddddd\n");
 		spent_ml = timer1_ticks * TIMER1_TICK_US * MILLILITERS_IN_MICROS;
-		printf("%u\n", LITTERS_IN_HOUR_FROM_MILLILITERS_IN_MICROS(spent_ml));
+		printf("%d\n", (int)spent_ml);
+		printf("%d\n", (int)LITTERS_IN_HOUR_FROM_MILLILITERS_IN_MICROS(spent_ml));
 
 		timer1_ticks = 0;
 	    timer2_ticks = 0;
