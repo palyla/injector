bf789567ebd4fb0798c16023b76d51a1c17fe01b
palyla <palyla@ya.ru>
2020-01-12 16:19:11 +0300

    Added android recording tool

---
+++ tools/csvmgr/app/src/main/java/com/injector/csvmgr/MainActivity.java
@@ -0,0 +1,55 @@
+package com.injector.csvmgr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ProbeTable customTable = new ProbeTable();
+//        customTable.addProduct(0x1234, 0x0001, CdcAcmSerialDriver.class);
+//        customTable.addProduct(0x1234, 0x0002, CdcAcmSerialDriver.class);
+//
+//        UsbSerialProber prober = new UsbSerialProber(customTable);
+
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            System.out.println("No USB devices were found!");
+        }
+        System.out.println(availableDrivers.toString());
+
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+            return;
+        }
+
+        UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        try {
+            port.open(connection);
+            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
