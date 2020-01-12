323760ff7972bfc4976f1b0342e1883a071455bb
palyla <palyla@ya.ru>
2020-01-12 20:10:38 +0300

    Updated main activity

---
+++ tools/csvmgr/app/src/main/java/com/injector/csvmgr/BoardPort.java
@@ -0,0 +1,5 @@
+package com.injector.csvmgr;
+
+public class BoardPort {
+
+}
--- tools/csvmgr/app/src/main/java/com/injector/csvmgr/MainActivity.java
+++ tools/csvmgr/app/src/main/java/com/injector/csvmgr/MainActivity.java
@@ -27,29 +27,28 @@ public class MainActivity extends AppCompatActivity {
 //
 //        UsbSerialProber prober = new UsbSerialProber(customTable);
 
-        // Find all available drivers from attached devices.
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty()) {
-            System.out.println("No USB devices were found!");
-        }
-        System.out.println(availableDrivers.toString());
-
-        // Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == null) {
-            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
-            return;
-        }
-
-        UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
-        try {
-            port.open(connection);
-            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+//        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+//        if (availableDrivers.isEmpty()) {
+//            System.out.println("No USB devices were found!");
+//        }
+//        System.out.println(availableDrivers.toString());
+//
+//        // Open a connection to the first available driver.
+//        UsbSerialDriver driver = availableDrivers.get(0);
+//        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+//        if (connection == null) {
+//            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+//            return;
+//        }
+//
+//        UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+//        try {
+//            port.open(connection);
+//            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
