90809825b26f26d16fd0910572c304c84b9677f1
Sedlyarsky, Alexey <Alexey.Sedlyarsky@emc.com>
2020-01-30 17:02:24 +0300

    Semaphore thoughts

--- app_v2.c
+++ app_v2.c
@@ -21,12 +21,14 @@
     #define fatal(fmt, ...)
 #endif
 
-#define _ERR_MSG_NO_HEAP "Insufficient FreeRTOS heap"
+#define _ERR_MSG_NO_HEAP    "Insufficient heap"
+#define _ERR_MSG_QUEUE_FULL "Queue is full"
 
 #define TASK_DEFAULT_PRIORITY 255
 #define TASK_DEFAULT_STACK_SIZE 256
 // #define TIMER_INTERVAL_MS 250
 #define TIMER_INTERVAL_MS 1000
+#define MUTEX_WAIT_TICKS 10
 
 
 static const char* data_msg =  "{"                                    \
@@ -59,12 +61,16 @@ static const char* integrity_msg =  "{"                         \
                                     "}\n";
 
 
-params_t xParams;
-stats_t xStats;
+
 TimerHandle_t xTimerInterval = NULL;
 TaskHandle_t xHandleTaskDisplay = NULL;
 TaskHandle_t xHandleTaskSerial = NULL;
-BaseType_t xReturned;
+SemaphoreHandle_t xSemData;
+
+params_t xParams;
+params_t xParamsCopy;
+stats_t xStats;
+stats_t xStatsCopy;
 
 
 static inline void vInterruptInjector(void);
@@ -85,13 +91,17 @@ ISR(INT1_vect) { vInterruptInjector(); }
 
 
 static void vEvaluate(void) {
-    /* critical start */
+    BaseType_t xReturned;
     float fPassPathM = 0.0;
     float fPassPathKm = 0.0;
     float fPassFuelMl = 0.0;
     float fPassFuelLit = 0.0;
     float fPassFuelRub = 0.0;
     float fElapsedM = 0.0; /* The sum of durations while an injector in open state [Minutes] */
+
+    xReturned = xSemaphoreTake(xSemData, (TickType_t) MUTEX_WAIT_TICKS);
+    if(xReturned != pdTRUE)
+        taskYIELD();
 
     fElapsedM = ((float)ullTicksInjector * TIMER1_TICK_US / (1000.0 * 1000.0 * 60.0)) * INJECTORS;
     fPassFuelMl = (fElapsedM * VOLUMETRIC_FLOW_MILLILITERS_IN_MINUTE);
@@ -119,23 +129,31 @@ static void vEvaluate(void) {
     xParams.fAirflowCel = 0.0;
     xParams.fEngineCel = 0.0;
     xParams.uEngineRpm = 0.0;
-    /* critical end */
+
+    xReturned = xSemaphoreGive(xSemData);
+    if(xReturned == pdFALSE) {
+        fatal("xSemData:xSemaphoreGive: " _ERR_MSG_QUEUE_FULL)
+    }
 }
 
 
 void vTimerIntervalCallback(TimerHandle_t xTimer) {
-    /* critical start */
-    uint64_t ullTicksInjectorCopy = ullTicksInjector;
-    uint64_t ullTicksWheelCopy = ullTicksWheel;
+    uint64_t ullTicksInjectorCopy;
+    uint64_t ullTicksWheelCopy;
 
+    taskENTER_CRITICAL();
+    ullTicksInjectorCopy = ullTicksInjector;
+    ullTicksWheelCopy = ullTicksWheel;
     vTiksReset();
+    taskEXIT_CRITICAL();
 
     vEvaluate();
-    /* critical end */
 }
 
 
 static inline void vInterruptInjector(void) {
+    taskENTER_CRITICAL_FROM_ISR();
+
     if (PIND & (1 << PD3)) {
         /* Prescaler 1024 and 16MHZ frequncy [64 micros ... 4.19424 secs] */
         TCCR1B |= (1 << CS12) | (1 << CS10);
@@ -144,10 +162,15 @@ static inline void vInterruptInjector(void) {
         TCNT1 = 0x0;
         TCCR1B = 0x0;
     }
+    taskEXIT_CRITICAL_FROM_ISR();
 }
 
 static inline void vInterruptWheel(void) {
+    taskENTER_CRITICAL_FROM_ISR();
+
     ullTicksWheel += 1;
+    
+    taskEXIT_CRITICAL_FROM_ISR();
 }
 
 static void vTiksReset(void) {
@@ -173,6 +196,13 @@ static void vInitHW(void) {
 }
 
 static void vInitOS(void) {
+    BaseType_t xReturned;
+
+    xSemData = xSemaphoreCreateMutex();
+    if(xSemData == NULL) {
+        fatal("xSemData: " _ERR_MSG_NO_HEAP);
+    }
+
     xTimerInterval = xTimerCreate(
         "xTimerInterval", 
         pdMS_TO_TICKS(TIMER_INTERVAL_MS), 
@@ -208,6 +238,11 @@ static void vInitOS(void) {
     if(xReturned != pdPASS) {
         fatal("vTaskSerial: " _ERR_MSG_NO_HEAP);
     }
+
+    xReturned = xTimerStart(xTimerInterval, 0);
+    if(xReturned != pdPASS) {
+        fatal("xTimerInterval: " _ERR_MSG_NO_HEAP);
+    }
 }
 
 void vTaskDisplay(void * pvParameters) {
@@ -217,38 +252,45 @@ void vTaskDisplay(void * pvParameters) {
 }
 
 void vTaskSerial(void * pvParameters) {
+    BaseType_t xReturned;
+
     while(1) {
+        xReturned = xSemaphoreTake(xSemData, (TickType_t) MUTEX_WAIT_TICKS);
+        if(xReturned != pdTRUE)
+            taskYIELD();
+        
+        xParamsCopy = xParams;
+        xStatsCopy = xStats;
+
+        xReturned = xSemaphoreGive(xSemData);
+        if(xReturned == pdFALSE) {
+            fatal("xSemData:xSemaphoreGive: " _ERR_MSG_QUEUE_FULL)
+        }
+
         printf(
             data_msg,
-            xParams.fPathKm,
-            xParams.fFuelLit,
-            xParams.fFuelRub,
-            xParams.fAirflowCel,
-            xParams.fEngineCel,
-            xParams.fSpeedKmPerHr,
-            xParams.fFuelLitPerHr,
-            xParams.fFuelRubPerHr,
-            xParams.fFuelLitPer100Km,
-            xParams.uEngineRpm,
-            xStats.fFuelLastWeekLit,
-            xStats.fFuelLastMonthLit,
+            xParamsCopy.fPathKm,
+            xParamsCopy.fFuelLit,
+            xParamsCopy.fFuelRub,
+            xParamsCopy.fAirflowCel,
+            xParamsCopy.fEngineCel,
+            xParamsCopy.fSpeedKmPerHr,
+            xParamsCopy.fFuelLitPerHr,
+            xParamsCopy.fFuelRubPerHr,
+            xParamsCopy.fFuelLitPer100Km,
+            xParamsCopy.uEngineRpm,
+            xStatsCopy.fFuelLastWeekLit,
+            xStatsCopy.fFuelLastMonthLit,
             "xPit"
         );
+
     }
 }
 
 
-
 int main(void) {
-
     vInitHW();
     vInitOS();
-
-    xReturned = xTimerStart(xTimerInterval, 0);
-    if(xReturned != pdPASS) {
-        fatal("xTimerInterval: " _ERR_MSG_NO_HEAP);
-    }
-    
     vTaskStartScheduler();
     
     /* We shouldn't be here */
